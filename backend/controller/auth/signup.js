f1c03f02ce2397ff5f92026c7733ba0e1c853c3e
devmike2 <michaelbello2.44@gmail.com>
2025-04-03 21:33:26 +0100

    production

--- backend/controller/auth/signup.js
+++ backend/controller/auth/signup.js
@@ -40,6 +40,7 @@ const userSignup = async (req, res) =>{
         const token = createToken(newUser._id)
         res.cookie('jwt', token, {
             httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
             maxAge: maxAge * 1000
         })
         res.status(201).json({
